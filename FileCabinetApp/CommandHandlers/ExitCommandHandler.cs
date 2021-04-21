7ed999d2264cf384ef45e1719cebd03328da700f
Никита Сидоренко <nikitastar280@gmail.com>
2021-04-21 13:29:18 +0300

    Field refactor isRunning

--- FileCabinetApp/CommandHandlers/ExitCommandHandler.cs
+++ FileCabinetApp/CommandHandlers/ExitCommandHandler.cs
@@ -11,6 +11,17 @@ namespace FileCabinetApp.CommandHandlers
     /// </summary>
     public class ExitCommandHandler : CommandHandlerBase
     {
+        private Action<bool> action;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExitCommandHandler"/> class.
+        /// </summary>
+        /// <param name="action">Action.</param>
+        public ExitCommandHandler(Action<bool> action)
+        {
+            this.action = action;
+        }
+
         /// <summary>
         /// Command handler.
         /// </summary>
@@ -25,7 +36,7 @@ namespace FileCabinetApp.CommandHandlers
             if (string.Equals(appCommandRequest.Command, "exit", StringComparison.InvariantCultureIgnoreCase))
             {
                 Console.WriteLine("Exiting an application...");
-                Program.isRunning = false;
+                this.action(false);
             }
             else
             {
--- FileCabinetApp/Program.cs
+++ FileCabinetApp/Program.cs
@@ -9,8 +9,8 @@ namespace FileCabinetApp
     /// </summary>
     public static class Program
     {
-        public static bool isRunning = true;
         public static IRecordValidator recordValidator;
+        private static bool isRunning = true;
         private const string DeveloperName = "Nikita Sidorenko";
         private const string HintMessage = "Enter your command, or enter 'help' to get help.";
         private static IFileCabinetService fileCabinetService;
@@ -67,7 +67,7 @@ namespace FileCabinetApp
             }
 
             var helpCommandHandler = new HelpCommandHandler();
-            var exitCommandHandler = new ExitCommandHandler();
+            var exitCommandHandler = new ExitCommandHandler(ExitProgram);
             var statCommandHandler = new StatCommandHandler(fileCabinetService);
             var createCommandHandler = new CreateCommandHandler(fileCabinetService);
             var listCommandHandler = new ListCommandHandler(fileCabinetService);
@@ -91,6 +91,11 @@ namespace FileCabinetApp
             removeCommandHandler.SetNext(purgeCommandHandler);
             purgeCommandHandler.SetNext(missedCommandHandler);
             return helpCommandHandler;
+        }
+
+        private static void ExitProgram(bool isRunning)
+        {
+            Program.isRunning = isRunning;
         }
 
         private static void LoadSettings(string[] args)
